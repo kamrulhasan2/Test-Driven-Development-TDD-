a4b597b5962b8745aba03a0d0e997b44e94e5aed
kamrulhasan2 <kamrulhasan20656@gmail.com>
2025-09-28 23:31:11 +0600

    implement the Navbar using test case

--- React-testing-library/Using-Next.js/my-app/src/app/components/Navbar.jsx
+++ React-testing-library/Using-Next.js/my-app/src/app/components/Navbar.jsx
@@ -1,9 +1,11 @@
+import Link from 'next/link';
 import React from 'react'
 
 const Navbar = () => {
   return (
     <nav>
-       
+        <Link href={'/'}>Home</Link>
+        <Link href={'/about'}>About</Link>
     </nav>
   )
 }
