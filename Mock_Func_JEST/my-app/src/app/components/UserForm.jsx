f7ea0d1ef0ea70123836c406f0fe7629aa7d4f8c
kamrulhasan2 <kamrulhasan20656@gmail.com>
2025-10-02 23:18:37 +0600

    according to the test case create userForm components

--- Mock_Func_JEST/my-app/src/app/components/UserForm.jsx
+++ Mock_Func_JEST/my-app/src/app/components/UserForm.jsx
@@ -1,7 +1,28 @@
+"use client"
+import { useState } from "react"
 
 const UserForm = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+        console.log(`Name: ${name}, Email: ${email}`);
+    }
+
   return (
-    <div>UserForm</div>
+    <div>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} />
+
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" value={email} onChange={(e)=> setEmail(e.target.value)} />
+
+            <button type="submit">Submit</button>
+        </form>
+    </div>
   )
 }
 
